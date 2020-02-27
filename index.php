ff41b433f78af4c05237f73b25e9d8bd522a319d
Dr. Bryan A. Cage <bacagedeveloper@gmail.com>
2020-02-27 08:24:16 -0500

    Update index.php

--- index.php
+++ index.php
@@ -3,74 +3,17 @@
 include_once 'functions.php';
 include_once 'resource/database.php';
 
-// Keep this API Key value to be compatible with the ESP32 code provided in the project page.
-// If you change this value, the ESP32 sketch needs to match
-$api_key_value = "tPmAT5Ab3j7F9";
+?>
 
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
 
-echo "</br>";
-pre_r("\$_GET Array");
-pre_r($_GET);
-
-$api_key = "";
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $vacancy = $_POST['vacancy'];
-    $equipment_id = $_POST['equipment_id'];
-
-    echo "api_key: " . $_POST["api_key"] . "</br>";
-    $api_key = test_input($_POST["api_key"]);
-
-    if ($api_key == $api_key_value) {
-        echo "<h4>Your api_key is correct</h4>";
-        echo "<p>INSERTING DATA TO YOUR DATABASE</p>";
-        $sql = "UPDATE db_urec_app.equipment SET vacancy = :vacancy WHERE equipment_id = :equipment_id";
-        $stmt = $db->prepare($sql);
-        $stmt->execute(array(':vacancy' => $vacancy, ':equipment_id' => $equipment_id));
-
-        if ($stmt->rowCount() == 1) {
-            echo "Database Updated Successfully.";
-        } else {
-            echo "Update was not successful";
-        }
-    } else {
-        echo "<h4>Your api_key is not correct.</h4>";
-        echo "<p>ACCESS DENIED => CANNOT CONNECT TO DATABASE</p>";
-    }
-}
-
-if ($_SERVER["REQUEST_METHOD"] == "GET") {
-    $vacancy = $_GET['vacancy'];
-    $equipment_id = $_GET['equipment_id'];
-
-    echo "api_key: " . $_GET["api_key"] . "</br>";
-    $api_key = test_input($_GET["api_key"]);
-
-    if ($api_key == $api_key_value) {
-        echo "<h4>Your api_key is correct</h4>";
-        echo "<p>INSERTING DATA TO YOUR DATABASE</p>";
-        $sql = "UPDATE db_urec_app.equipment SET vacancy = :vacancy WHERE equipment_id = :equipment_id";
-        $stmt = $db->prepare($sql);
-        $stmt->execute(array(':vacancy' => $vacancy, ':equipment_id' => $equipment_id));
-
-        if ($stmt->rowCount() == 1) {
-            echo "Database Updated Successfully.";
-        } else {
-            echo "Update was not successful";
-        }
-
-    } else {
-        echo "<h4>Your api_key is not correct.</h4>";
-        echo "<p>ACCESS DENIED => CANNOT CONNECT TO DATABASE</p>";
-    }
-
-}
-
-function test_input($data)
-{
-    $data = trim($data);
-    $data = stripslashes($data);
-    $data = htmlspecialchars($data);
-    return $data;
-}
-
+</body>
+</html>
